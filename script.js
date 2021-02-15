1951a8c67e7628522dcb33c8fd33ddc7c49a20d7
Annemette <anne7n62@stud.kea.dk>
2021-02-15 19:46:34 +0100

    Getting HEX works

--- script.js
+++ script.js
@@ -1,48 +1,88 @@
 "use strict";
 
-window.document.querySelector("DOMContentLoaded", start);
+window.addEventListener("DOMContentLoaded", start);
 
 function start(){
 console.log("start");
 
-test();
+readInput();
 }
 
-function test() {
-    r /= 255;
-   g /= 255;
-   b /= 255;
- 
-   let h, s, l;
- 
-   const min = Math.min(r,g,b);
-   const max = Math.max(r,g,b);
-  
-   if( max === min ) {
-     h = 0;
-   } else
-   if (max === r) {
-     h = 60 * (0 + (g - b) / (max - min) );
-   } else
-   if (max === g) {
-     h = 60 * (2 + (b - r) / (max - min) );
-   } else
-   if (max === b) {
-     h = 60 * (4 + (r - g) / (max - min) );
-   }
-  
-   if (h < 0) {h = h + 360; }
-  
-   l = (min + max) / 2;
-  
-   if (max === 0 || min === 1 ) {
-     s = 0;
-   } else {
-     s = (max - l) / ( Math.min(l,1-l));
-   }
-   // multiply s and l by 100 to get the value in percent, rather than [0,1]
-   s *= 100;
-   l *= 100;
- 
-   console.log("hsl(%f,%f%,%f%)", h, s, l); // just for testing
+function readInput(){ // reading the input and getting the HEX
+    console.log("reading input");
+    let input = document.querySelector(".colorpicker"); //getting the input
+    let colorValue = input.value; // variable colorValue is the value of the input
+    
+    document.querySelector(".HEXoutput").textContent = colorValue; //places the colorvalue in the DOM
+    console.log(`the HEX is ${colorValue}`);
+
+    calRGB(colorValue);
+    }
+
+function calRGB(colorValue){
+            console.log("calculation RGB");
+            
+            displayRGB();
 }
+    
+function calHSL(){
+            console.log("calculation HSL");
+                
+            displayHSL();
+            }
+
+
+function displayHEX(){
+console.log("display HEX");
+
+
+
+}
+
+function displayRGB(){
+    console.log("display RGB");
+}
+
+function displayHSL(){
+    console.log("HSL")
+}
+
+
+// function test() {
+//     r /= 255;
+//    g /= 255;
+//    b /= 255;
+ 
+//    let h, s, l;
+ 
+//    const min = Math.min(r,g,b);
+//    const max = Math.max(r,g,b);
+  
+//    if( max === min ) {
+//      h = 0;
+//    } else
+//    if (max === r) {
+//      h = 60 * (0 + (g - b) / (max - min) );
+//    } else
+//    if (max === g) {
+//      h = 60 * (2 + (b - r) / (max - min) );
+//    } else
+//    if (max === b) {
+//      h = 60 * (4 + (r - g) / (max - min) );
+//    }
+  
+//    if (h < 0) {h = h + 360; }
+  
+//    l = (min + max) / 2;
+  
+//    if (max === 0 || min === 1 ) {
+//      s = 0;
+//    } else {
+//      s = (max - l) / ( Math.min(l,1-l));
+//    }
+//    // multiply s and l by 100 to get the value in percent, rather than [0,1]
+//    s *= 100;
+//    l *= 100;
+ 
+//    console.log("hsl(%f,%f%,%f%)", h, s, l); // just for testing
+// }
