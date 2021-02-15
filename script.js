b00dc0d74f21a0b81bda0e0be2a192007085858b
Annemette <anne7n62@stud.kea.dk>
2021-02-15 20:19:10 +0100

    it works, except for the instant shift of color

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ console.log("start");
 readInput();
 }
 
+// HVORDAN FÅR JEG DEN TIL AT FANGE INSTANT??? Everytime the user changes the color, the webpage must update the display
 function readInput(){ // reading the input and getting the HEX
     console.log("reading input");
     let input = document.querySelector(".colorpicker"); //getting the input
@@ -16,73 +17,71 @@ function readInput(){ // reading the input and getting the HEX
     document.querySelector(".HEXoutput").textContent = colorValue; //places the colorvalue in the DOM
     console.log(`the HEX is ${colorValue}`);
 
-    calRGB(colorValue);
+    calRGB(colorValue); //sending the parameter to function
     }
 
-function calRGB(colorValue){
+function calRGB(colorValue) {
             console.log("calculation RGB");
             
-            displayRGB();
+    //dividing the hex into 3 part for the rgb - shipping the #
+    const r = colorValue.substring(1,3);
+    const g = colorValue.substring(3,5);
+    const b = colorValue.substring(5,7);
+
+    //make into the numbers for rgb
+    const r2 = Number.parseInt(r,16);
+    const g2 = Number.parseInt(g,16);
+    const b2 = Number.parseInt(b,16);
+    const rgbresult = `(${r2},${g2},${b2})`;
+
+    //placing the result in the DOM
+    document.querySelector(".RGBoutput").textContent = rgbresult;
+    console.log(`rgb koden er (${r2},${b2},${g2})`)
+
+            calHSL(r2,g2,b2);
 }
     
-function calHSL(){
+function calHSL(r2,g2,b2){ //converting rgb to hsl
             console.log("calculation HSL");
                 
-            displayHSL();
+            r2 /= 255;
+            g2 /= 255;
+            b2 /= 255;
+          
+            let h, s, l;
+          
+            const min = Math.min(r2,g2,b2);
+            const max = Math.max(r2,g2,b2);
+           
+            if( max === min ) {
+              h = 0;
+            } else
+            if (max === r2) {
+              h = 60 * (0 + (g2 - b2) / (max - min) );
+            } else
+            if (max === g2) {
+              h = 60 * (2 + (b2 - r2) / (max - min) );
+            } else
+            if (max === b2) {
+              h = 60 * (4 + (r2 - g2) / (max - min) );
+            }
+           
+            if (h < 0) {h = h + 360; }
+           
+            l = (min + max) / 2;
+           
+            if (max === 0 || min === 1 ) {
+              s = 0;
+            } else {
+              s = (max - l) / ( Math.min(l,1-l));
+            }
+            // multiply s and l by 100 to get the value in percent, rather than [0,1]
+            s *= 100;
+            l *= 100;
+          
+            document.querySelector(".HSLoutput").textContent = `${h}, ${s}, ${l}`;
+            console.log("hsl(%f,%f%,%f%)", h, s, l); // just for testing
+           //SPG: HVORDAN FORKORTER JEG KOMME TALLENE???
             }
 
 
-function displayHEX(){
-console.log("display HEX");
-
-
-
-}
-
-function displayRGB(){
-    console.log("display RGB");
-}
-
-function displayHSL(){
-    console.log("HSL")
-}
-
-
-// function test() {
-//     r /= 255;
-//    g /= 255;
-//    b /= 255;
- 
-//    let h, s, l;
- 
-//    const min = Math.min(r,g,b);
-//    const max = Math.max(r,g,b);
-  
-//    if( max === min ) {
-//      h = 0;
-//    } else
-//    if (max === r) {
-//      h = 60 * (0 + (g - b) / (max - min) );
-//    } else
-//    if (max === g) {
-//      h = 60 * (2 + (b - r) / (max - min) );
-//    } else
-//    if (max === b) {
-//      h = 60 * (4 + (r - g) / (max - min) );
-//    }
-  
-//    if (h < 0) {h = h + 360; }
-  
-//    l = (min + max) / 2;
-  
-//    if (max === 0 || min === 1 ) {
-//      s = 0;
-//    } else {
-//      s = (max - l) / ( Math.min(l,1-l));
-//    }
-//    // multiply s and l by 100 to get the value in percent, rather than [0,1]
-//    s *= 100;
-//    l *= 100;
- 
-//    console.log("hsl(%f,%f%,%f%)", h, s, l); // just for testing
-// }
